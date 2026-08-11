347dcce956908b96d1269f7ff3d07ccf3df6441c
Ben Ramage <benramage31@gmail.com>
2026-08-11 14:34:53 +1000

    Minor route updates.

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,10 @@
+import 'package:firebase_auth_starter/features/profile/presentation/pages/profile_page.dart';
 import 'package:firebase_auth_starter/utils/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,34 +12,7 @@ class App extends StatelessWidget {
       title: 'Firebase Auth Starter',
       theme: AppTheme.lightTheme,
       themeMode: ThemeMode.light,
-      home: const HomeScreen(title: 'Firebase Auth Starter'),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(mainAxisAlignment: .center, children: [
-          ],
-        ),
-      ),
+      home: const ProfilePage(title: 'Firebase Auth Starter'),
     );
   }
 }
